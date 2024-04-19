3ceeddf80e7d3c402980c0e5703c5bca6a0b7666
Don_Aveo <133364365+BrendonAvelino@users.noreply.github.com>
2024-04-19 14:48:58 -0300

    Initial commit

---
+++ ex03/ex03.cpp
@@ -0,0 +1,78 @@
+#include <iostream> 
+using namespace std;
+
+	/*
+
+// ex01
+	int main() {
+	double numero1;
+	double numero2;
+	cout << "Informe dois numeros. \n";
+	cout << "Numero 1:";
+	cin >> numero1;
+	cout << "Numero 2:";
+	cin >> numero2;
+
+	if (numero1 > numero2) {
+		cout << "O numero " << numero1 << " e maior que o numero " << numero2;
+	}
+	else if (numero2 > numero1) {
+		cout << "O numero " << numero2 << " e maior que o numero " << numero1;
+	}
+	else {
+		cout << "os número são iguais";
+	}
+}
+
+	*/
+
+	
+	
+
+
+	
+	/*
+//ex02
+	int main() {
+		int estoque;
+		cout << "Informe a quantidade de produtos que existem no estoque... \n";
+		cout << "Quantidade: ";
+		cin >> estoque;
+		if (estoque >= 100) {
+			cout << "Quantidade suficiente no estoque";
+		}
+		else if (estoque >= 50) {
+			cout << "Atencao ao estoque";
+		}
+		else {
+			cout << "Faca um novo pedido e reabasteca o estoque";
+		}
+
+	}
+
+	*/
+
+
+
+
+
+	/*
+//ex03 
+	int main() {
+		int numero;
+
+		cout << "Informe um numero: ";
+		cin >> numero;
+		if (numero > 0) {
+			cout << "O numero " << numero << " e positivo.";
+		}
+		else if (numero < 0) {
+			cout << "O numero " << numero << " e negativo.";
+		}
+		else {
+			cout << "O numero " << numero;
+		}
+
+
+	}
+	*/
